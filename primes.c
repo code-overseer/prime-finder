62a9a0c441648dfbcf9ef9059e78074b61eb9c23
muramasa2402 <wongjengyan@gmail.com>
2020-08-23 19:35:39 +0100

    tabs to spaces

--- primes.c
+++ primes.c
@@ -7,33 +7,33 @@ int const prime_array[10000] = PRIME_10000;
 
 static int find_bigger_prime(int n)
 {
-	n |= 0x1;
-	for (int i = 1; i < 10000 && prime_array[i] * prime_array[i] <= n; ++i)
-	{
-		char b = !(n % prime_array[i]);
-		n += b * 2;
-		i = b + !b * i;
-	}
+    n |= 0x1;
+    for (int i = 1; i < 10000 && prime_array[i] * prime_array[i] <= n; ++i)
+    {
+        char b = !(n % prime_array[i]);
+        n += b * 2;
+        i = b + !b * i;
+    }
 
-	return n;
+    return n;
 }
 
 int find_next_prime(int const n)
 {
-	if (n <= prime_array[0]) return prime_array[0];
-	if (n >= prime_array[9999]) return find_bigger_prime(n);
-	int min = 0;
-	int max = 9999;
-	
-	while ((max - min) > 1)
-	{
-		int mid = (min + max) >> 1;
-		char b = n < prime_array[mid];
-		min = !b * mid + b * min;
-		max = b * mid + !b * max;
-	}
+    if (n <= prime_array[0]) return prime_array[0];
+    if (n >= prime_array[9999]) return find_bigger_prime(n);
+    int min = 0;
+    int max = 9999;
+    
+    while ((max - min) > 1)
+    {
+        int mid = (min + max) >> 1;
+        char b = n < prime_array[mid];
+        min = !b * mid + b * min;
+        max = b * mid + !b * max;
+    }
 
-	return prime_array[max];
+    return prime_array[max];
 }
 
 
